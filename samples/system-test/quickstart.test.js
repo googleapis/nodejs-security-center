12ba334b2aef04a8cf973863e611cb0fcebb8acc
Justin Beckwith <justin.beckwith@gmail.com>
2019-08-25 19:17:29 -0700

    refactor: drop dependency on execa (#131)

--- samples/system-test/quickstart.test.js
+++ samples/system-test/quickstart.test.js
@@ -1,17 +1,16 @@
-/**
- * Copyright 2019, Google, LLC.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 'use strict';
 
@@ -23,7 +22,7 @@ const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 // Skipped because createSource requires special permissions only
 // grantable on the organization level.
-it.skip('should run the quickstart', async () => {
+it.skip('should run the quickstart', () => {
   const stdout = execSync(`node quickstart ${organization}`);
   assert.match(stdout, /Source created\./);
 });
