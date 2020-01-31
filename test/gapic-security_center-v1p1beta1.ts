06df202e65ddf5d8e9514439b27a7d4c9145d7e2
Benjamin Coe <bencoe@google.com>
2020-01-31 11:38:20 -0800

    chore: header lint

--- test/gapic-security_center-v1p1beta1.ts
+++ test/gapic-security_center-v1p1beta1.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -328,7 +328,7 @@ describe('v1p1beta1.SecurityCenterClient', () => {
       const expectedResponse = {};
       // Mock gRPC layer
       client._innerApiCalls.getIamPolicy = mockSimpleGrpcMethod(
-        request,
+        request,sc
         null,
         error
       );
