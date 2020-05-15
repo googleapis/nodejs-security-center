9b9f9fa01f96aa669fc55df061765ca3829fe574
hannah-tsai <hannahtsai@google.com>
2020-05-15 10:01:48 -0700

    Add filter field libary sample for UpdateNotificationConfig

--- samples/v1/updateNotificationConfig.js
+++ samples/v1/updateNotificationConfig.js
@@ -36,12 +36,14 @@ function main(
 
   async function updateNotificationConfig() {
     const [response] = await client.updateNotificationConfig({
-      updateMask: {paths: ['description', 'pubsub_topic', 'streaming_config.filter']},
+      updateMask: {
+        paths: ['description', 'pubsub_topic', 'streaming_config.filter'],
+      },
       notificationConfig: {
         name: formattedConfigName,
         description: 'Updated config description',
         pubsubTopic: pubsubTopic,
-        streaming_config: {filter: 'state = \"INACTIVE\"'}
+        streaming_config: {filter: 'state = "INACTIVE"'},
       },
     });
     console.log('notification config update succeeded: ', response);
